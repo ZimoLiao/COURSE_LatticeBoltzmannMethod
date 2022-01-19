7ab5ade76467fa90e4dd7ee60c735223805a6c19
Zimo <zimoLiao@mail.ustc.edu.cn>
2022-01-19 17:38:02 +0800

    update

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,22 +1,43 @@
+#include<mpi.h>
+#include<iostream>
 
 #include"LatticeMoment.h"
 #include"LatticePopulation.h"
 
+
+using namespace std;
+
 int main()
 {
+	constexpr int host = 0;
+	int size, rank;
+	MPI_Status status;
+
+	MPI_Init(NULL, NULL);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == host) {
+		cout << "===========================================================\n";
+		cout << "LBSolver-Parallel start...\n";
+		cout << "===========================================================\n";
+	}
+
+	cout << "Processor " << rank << " started.\n";
 
 	LatticePopulation lp;
 	double m0[3] = { 1.0,0.0,0.0 };
-	LatticeMoment lm(100, 120, m0);
+	LatticeMoment lm(20, 20, m0);
 
 	lm.SetVelocityShear(0.1);
-	lm.OutputAscii("output2/output_0.dat");
+	//lm.OutputAscii("output2/output_0.dat");
 
 	lp.Init(lm);
 
 	int step = 0;
 	string fname;
-	for (int i = 0; i != 4000; i++) {
+	for (int i = 0; i != 0; i++) {
 
 		step++;
 
@@ -28,9 +49,16 @@ int main()
 
 		lm.Update(lp);
 
-		if (step % 50 == 0) {
-			fname = "output2/output_" + to_string(step) + ".dat";
-			lm.OutputAscii(fname);
+		if (step % 10 == 0) {
+			cout << "step " << step << endl;
+
+			//fname = "output2/output_" + to_string(step) + ".dat";
+			//lm.OutputAscii(fname);
 		}
 	}
+
+
+	MPI_Finalize();
+
+	return 0;
 }
